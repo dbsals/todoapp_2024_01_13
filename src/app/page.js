77fd2b44134d838667136d97b79c0a26b721bdac
YoonMin <dbsals999@naver.com>
2024-01-13 16:02:52 +0900

    프리티어 세팅

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,25 @@
-import Image from 'next/image'
+"use client"
 
+import Head from 'next/head'
+import Link from 'next/link'
 export default function Home() {
   return (
-    <main>
-      <div>
-        환영합니다.
-      </div>
-    </main>
-  )
+    <>
+      <Head>        
+        <meta charset="UTF-8" />	            
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>  
+      <h1>앱 소개 페이지</h1>
+
+      <nav>
+        <Link href="/" legacyBehavior>
+          <a className="text-red-300">메인</a>
+        </Link>
+
+        <Link href="/about" legacyBehavior>
+          <a className="text-blue-300">어바웃</a>
+        </Link>
+      </nav> 
+    </>  
+  );
 }
