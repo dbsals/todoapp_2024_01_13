f648b9018d97ab54d06c7f6cd27d47fccc77ca71
YoonMin <dbsals999@naver.com>
2024-01-14 14:11:18 +0900

    button 적용, icon 테스트

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,33 @@
 
 import { ThemeProvider } from '@mui/material/styles';
 import { Button, Box, AppBar, Toolbar } from '@mui/material';
+import { MdDelete } from 'react-icons/md';
 import theme from './theme';
 
 export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <Button variant="contained">버튼</Button>
+        <div className="tw-flex tw-items-center tw-gap-x-3">
+          <Button variant="text">Text</Button>
+          <Button variant="contained">Contained</Button>
+          <Button variant="outlined">Outlined</Button>
+        </div>
+
+        <div className="tw-flex tw-items-center tw-gap-x-3 tw-mt-3">
+          <Button variant="text" onClick={() => alert('버튼이 클릭되었습니다.')}>
+            Text
+          </Button>
+          <Button variant="contained" disabled>
+            Contained
+          </Button>
+          <Button variant="outlined" href="/sub">
+            Sub 이동
+          </Button>
+          <Button variant="contained" endIcon={<MdDelete />}>
+            삭제
+          </Button>
+        </div>
       </ThemeProvider>
     </>
   );
---
+++ src/app/sub/page.js
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Sub() {
+  return (
+    <>
+      <h1> 서브페이지입니다. </h1>{' '}
+    </>
+  );
+}
